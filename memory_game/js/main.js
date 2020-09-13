6bae259738f8800825737edf0bc1a72e240d239e
Overstreet James <OverstreetJames@JohnDeere.com>
2020-09-13 16:17:47 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,26 @@
-//console.log("User flipped " + cardOne);
-// console.log("User flipped " + cardFour);
 
-
-const cards = ["Queen", "Queen", "King", "King"];
+const cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
 const cardsInPlay = [];
 
 function checkForMatch (){
@@ -15,9 +33,11 @@ function checkForMatch (){
 }
 function flipCard(cardId){
 
-console.log("User flipped " + cards[cardId])
-cardsInPlay.push(cards[cardId]);
+console.log("User flipped " + cards[cardId].rank)
+cardsInPlay.push(cards[cardId].rank);
 cardsInPlay;
+console.log(cards[cardId].suit);
+console.log(cards[cardId].cardImage);
 
 if ( cardsInPlay.length === 2){
 	checkForMatch();
@@ -28,12 +48,4 @@ flipCard(0);
 flipCard(2);
 
 
-//let cardOne = cards[0];
-//cardsInPlay.push(cardOne);
-//cardsInPlay;
-//console.log("User flipped " + cardOne);
 
-//let cardTwo = cards[2];
-//cardsInPlay.push(cardTwo);
-//cardsInPlay; 
-//console.log("User flipped " + cardTwo);
