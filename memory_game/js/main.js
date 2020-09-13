6489f206ec40ba5eb65e1d1279e2fbc0d8dc4e3e
Overstreet James <OverstreetJames@JohnDeere.com>
2020-09-13 08:43:45 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,26 @@
 console.log("Up and running!");
-let cardOne = "Queen";
-let cardTwo = "Queen";
-let cardThree = "King";
-let cardFour = "King";
+//console.log("User flipped " + cardOne);
+// console.log("User flipped " + cardFour);
 
+
+const cards = ["Queen", "Queen", "King", "King"];
+const cardsInPlay = [];
+
+
+let cardOne = cards[0];
+cardsInPlay.push(cardOne);
+cardsInPlay;
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+
+let cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+cardsInPlay; 
+console.log("User flipped " + cardTwo);
+
+if ( cardsInPlay.length === 2){
+	if (cardsInPlay[0] === cardsInPlay[1]){
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+} 
